51964f28ec369aebe8386e838d38ede89278c815
alexmisk <alexmisk@ya.ru>
2021-09-21 21:38:27 +0300

    header/hero/footer

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,10 +5,14 @@ module.exports = {
 	},
 	darkMode: false, // or 'media' or 'class'
 	theme: {
-		extend: {},
+		extend: {
+			fontFamily: {
+				effra: ["Effra"],
+			},
+		},
 	},
 	variants: {
 		extend: {},
 	},
-	plugins: [],
+	plugins: [require("daisyui")],
 };
